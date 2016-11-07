2c4c4f5b604c31fd5d121db920577fc1f9f9a2c5
sofiamatulis <sosomatulis@hotmail.com>
2016-11-06 19:37:42 -0500

    changed response to feature popular

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def index
   client = F00px::Client.new
-  response = client.get('photos?rpp=100')
+  response = client.get('photos?rpp=100&feature=popular')
   @pictures  = JSON.parse(response.body)['photos']
 
       #retrieving the photos from the response in JSON
