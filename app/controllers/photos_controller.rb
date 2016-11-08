101972124c815829256461d009181a7719484ea4
sofiamatulis <sosomatulis@hotmail.com>
2016-11-08 13:55:48 -0500

    cleaned code and only show like button when logged in

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,4 @@
 class PhotosController < ApplicationController
-
   before_action :current_client
 
   def index
@@ -8,28 +7,27 @@ class PhotosController < ApplicationController
   #retrieving the photos that are popular and number of photos is equal to 100
   response = @client.get('photos?rpp=100&feature=popular')
   @pictures  = JSON.parse(response.body)['photos']
-
-      #retrieving the photos from the response in JSON and saving in a variable pictures that is used in the html file
-      #extracting the photos data from the json response , instead of all the information
+    #retrieving the photos from the response in JSON and saving in a variable pictures that is used in the html file
+    #extracting the photos data from the json response , instead of all the information
   end
-
 
   def like
     @picture   = params[:picture]
     # params of the picture is what is inside of the photo (information gotten from the URL)
     @client.post("photos/#{@picture}/vote?vote=1")
+    #post a vote (like) in the picture
     redirect_to root_path
     # https://github.com/500px/api-documentation/blob/master/endpoints/photo/POST_photos_id_vote.md
-    # vote option gotten from here
-
-
+    # vote docs gotten from here
   end
+
+
 
   private
   def current_client
     #using F00px gem
     @client = F00px::Client.new
-    #if the user is logged in, the token from the 500px account is the token for this session (they are the samew)
+    #if the user is logged in, the token from the 500px account is the token for this session (they are the same and I can like it from my 500px account)
     if session[:token_secret] && session[:token]
     @client.token           = session[:token]
     @client.token_secret    = session[:token_secret]
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
   root 'photos#index'
 
+  get '/auth/:provider/callback', to: 'sessions#create'
+  # when going to http://localhost:3000/auth/500px, bring to sessions create
 
-   get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-   delete '/logout', to: 'sessions#destroy'
-
-   get   '/like/:picture', to: 'photos#like'
-
-
-   # when going to http://localhost:3000/auth/500px, bring to sessions create
+  get   '/like/:picture', to: 'photos#like'
 
 
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
