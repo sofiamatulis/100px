b34a0dea2e6ff84ebd72ff75ca75eb625c4ca28d
sofiamatulis <sosomatulis@hotmail.com>
2016-11-09 16:50:25 -0500

    heroku secret key

--- Gemfile
+++ Gemfile
@@ -18,7 +18,6 @@ gem 'coffee-rails', '~> 4.2'
 
 gem 'f00px'
 gem 'omniauth-500px'
- gem 'heroku_secrets', github: 'alexpeattie/heroku_secrets'
 # gem 'rails_12factor', group: :production
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+  config.secret_key_base = ENV[“SECRET_KEY_BASE”]
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
 
