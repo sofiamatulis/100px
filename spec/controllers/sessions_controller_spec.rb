a97dd187082d5b48867949dc33d866ff2a261fbb
sofiamatulis <sosomatulis@hotmail.com>
2016-11-12 13:52:57 -0500

    created login test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,14 +1,31 @@
 require 'rails_helper'
 
+OmniAuth.config.test_mode = true
+
+# before do
+#   Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:F00px]
+# end
+
+OmniAuth.config.mock_auth[:F00px] = OmniAuth::AuthHash.new({
+  :provider => '500px',
+  :uid => '123545',
+  :name => 'Maria',
+  :token => 'dklamdkmq',
+  :secret => 'dkndnendew'
+})
+
+
+
 RSpec.describe SessionsController do
-  # 
-  # describe "guest access" do
-  #     describe 'GET #create' do
-  #     it "requires login" do
-  #     get :create
-  #     expect(response).to redirect_to root_path
-  #     end
-  #   end
-  # end
+
+  describe "guest access" do
+      describe 'GET #create' do
+      it "requires login" do
+      get :create, params: {provider: '500px'}
+      #redirecting to 500px log-in 
+      expect(response).to have_http_status(302)
+      end
+    end
+  end
 
 end
